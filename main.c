28be2fb81f3aec1614494af738657baf33af6413
harry g <duckmancan@gmail.com>
2019-04-27 04:18:37 +0000

    My code works!!! Now on to commenting and trying the decryption of rotation ciphers without a key!

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+
+
+//    char str[200];
+//    int i;
+//    FILE *input;
+//    input = fopen("input.txt", "r");
+//    if(input == NULL){
+//        perror("fopen()");
+//        return 0;
+//    }
+//    for (i = 0; !feof(input); i++){
+//        fscanf(input, "%[^\n]s", str);
+//        printf("%s", str);
+//    }
+
+
 void rot_enc(void);
 void rot_dec(void);
 void sub_enc(void);
+void sub_dec(void);
 int main()
 {
     int option;
@@ -22,42 +39,72 @@ int main()
         case 3:
             sub_enc();
         break;
-//        case 4;
-//        break;
+        case 4:
+            sub_dec();
+        break;
         default:
             printf("\nERROR: INVALID OPTION NUMBER");
     }
 }
 void rot_enc(void){
     int x, i, k;
+    char fileName[50];
     char str[200];
-    printf("\nPlease enter the plain text you wish to encrypt: ");
-    scanf("%s", str);
+    printf("\nPlease enter the name of the text file containing the plain text you wish to encrypt: ");
+    scanf("%s",fileName);
     printf("\nEnter the key with which you wish to encrypt (a number from 1-25): ");
     scanf("%d", &k);
     printf("\nYour encrypted text is: ");
-    for(i = 0; str[i] != '\0'; i++){ 
-        if(str[i] <= 90 && str[i] >= 65){          
+    
+    FILE *input;
+    input = fopen(fileName, "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
+    }       
+    while(!feof(input))
+    {
+                fscanf(input, "%[^\n]s", str);
+    }
+    for(i = 0; str[i] != '\0'; i++)
+    {         
+        if(str[i] <= 90 && str[i] >= 65)
+        {          
             x = str[i] - 65;
             x = (x + k)%26;
             x = x + 65;
             printf("%c", x);
         }
-        else{
+        else
+        {
             printf("%c", str[i]);
         }
     }
     printf("\n");
+        
 }
 
 void rot_dec(void){
     int x, i, k;
     char str[200];
-    printf("\nPlease enter the encrypted text you wish to decrypt: ");
-    scanf("%s", str);
+    char fileName[50];
+    printf("\nPlease enter the name of the encrypted text file you wish to decrypt: ");
+    scanf("%s",fileName);
     printf("\nEnter the key used to encrypt the text (a number from 1-25): ");
     scanf("%d", &k);
     printf("\nYour decrypted text is: ");
+    FILE *input;
+    input = fopen(fileName, "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
+    }       
+    while(!feof(input))
+    {
+        fscanf(input, "%[^\n]s", str);
+    }
     for(i = 0; str[i] != '\0'; i++)
     {  
         if(str[i] <= 90 && str[i] >= 65)
@@ -88,11 +135,23 @@ void sub_enc(void){
     int x, i;
     char sub[25];
     char str[200];
-    printf("\nPlease enter the plain text you wish to encrypt: ");
-    scanf("%s", str);
-    printf("\nEnter the substitution key you wish to use to encrypt the text (26 unique capital letters): ");
+    char fileName[50];
+    printf("\nPlease enter the name of the plain text file you wish to encrypt: ");
+    scanf("%s",fileName);
+    printf("\nEnter the substitution key you wish to use to encrypt the text (26 unique capital letters and no spaces): ");
     scanf("%s", sub);
-    printf("\nYour decrypted text is: ");
+    printf("\nYour encrypted text is: ");
+    FILE *input;
+    input = fopen(fileName, "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
+    }       
+    while(!feof(input))
+    {
+                fscanf(input, "%[^\n]s", str);
+    }
     for(i = 0; str[i] != '\0'; i++)
     { 
         if(str[i] <= 90 && str[i] >= 65)
@@ -101,6 +160,51 @@ void sub_enc(void){
             printf("%c", sub[x]);
         }
         else
+        {
+            printf("%c", str[i]);
+        }
+    }
+    printf("\n");
+}
+
+void sub_dec(void){
+    int x, i, n, j;
+    char sub[25];
+    char str[200];
+    char fileName[50];
+    printf("\nPlease enter the name of the encrypted text file you wish to decrypt: ");
+    scanf("%s",fileName);
+    printf("\nEnter the substitution key used to encrypt the text (26 unique capital letters): ");
+    scanf("%s", sub);
+    printf("\nYour encrypted text is: ");
+    FILE *input;
+    input = fopen(fileName, "r");
+    if(input == NULL)
+    {
+        perror("fopen()");
+        return;
+    }       
+    while(!feof(input))
+    {
+                fscanf(input, "%[^\n]s", str);
+    }
+    for (i = 0; str[i] != '\0'; i++)  
+    {
+      if(str[i] <= 90 && str[i] >= 65)
+        {
+            for(n = 0; n <= 26; n++)
+            {
+        
+                x = str[i] - 65;
+                j = sub[n] - 65;
+                if (x == j)
+                {
+                    j = n + 65;
+                    printf("%c", j);
+                }
+            }    
+        }
+        else 
         {
             printf("%c", str[i]);
         }
